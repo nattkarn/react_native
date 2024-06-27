dc85fc90149c0cc0b01349993cbe41489a3efd1f
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-06-27 16:25:41 +0700

    Components

--- App.js
+++ App.js
@@ -1,32 +1,14 @@
-import { Text, View, Image, Button, Alert } from "react-native";
+import { View  } from "react-native";
 import { myStyle } from "./styles/myStyle";
-import { useState } from "react";
-
-const logo = require("./assets/cat.jpg");
+import Person from './components/Persons'
 
 export default function App() {
-  const [developer, setDeveloper] = useState ({ name:"Nattkarn", year: 2024});
   
-
   return (
     <View style={myStyle.container}>
-      <Image source={require("./assets/cat.jpg")} style={myStyle.image}></Image>
-      <Text style={myStyle.header}>Hello React Native!!</Text>
-      <Text style={myStyle.content}>Creator: {developer.name} | {developer.year}</Text>
-      <Image
-        source={{
-          uri: "https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I",
-        }}
-        style={myStyle.image}
-      ></Image>
-      <Button
-        title="Update Detail"
-        color={"red"}
-        onPress={() => {
-          console.log("If See this. Button is Work");
-          setDeveloper({ name:"ARM" , year: 2567});
-        }}
-      ></Button>
+      <Person/>
+      <Person/>
+      <Person/>
     </View>
   );
 }
---
+++ components/Persons.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+
+
+export default function person() {
+    return (
+        <View>
+            <Text>ข้อมูลประชากร</Text>
+        </View>
+    )
+}
