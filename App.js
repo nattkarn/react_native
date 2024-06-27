94281d041a03ab95885f38770679c29138f6395d
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-06-27 15:32:50 +0700

    image

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+const logo = require("./assets/cat.jpg")
+
+
 
 export default function App() {
   return (
     <View style={myStyle.container}>
+      <Image source={require("./assets/cat.jpg")} style = {myStyle.image}></Image>
       <Text style={myStyle.header}>Hello React Native!!</Text>
       <Text style={myStyle.content}>Creator: Nattkarn</Text>
+      <Image source={{uri:"https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I"}} style = {myStyle.image}></Image>
     </View>
   );
 }
@@ -15,7 +20,6 @@ const myStyle = StyleSheet.create({
     flexDirection: "colum",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: 'orange'
   },
   header: {
     color: "red",
@@ -25,4 +29,8 @@ const myStyle = StyleSheet.create({
   content: {
     fontStyle: "italic",
   },
+  image: {
+    width:300,
+    height: 300
+  }
 });
