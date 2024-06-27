10544298867ecda9b432ef1a9b2ff30fee6e1ee3
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-06-27 16:15:09 +0700

    States

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
-import { Text, View, Image, Button, Alert} from "react-native";
+import { Text, View, Image, Button, Alert } from "react-native";
 import { myStyle } from "./styles/myStyle";
+import { useState } from "react";
 
 const logo = require("./assets/cat.jpg");
 
 export default function App() {
+  const [developer, setDeveloper] = useState ({ name:"Nattkarn", year: 2024});
+  
+
   return (
     <View style={myStyle.container}>
       <Image source={require("./assets/cat.jpg")} style={myStyle.image}></Image>
       <Text style={myStyle.header}>Hello React Native!!</Text>
-      <Text style={myStyle.content}>Creator: Nattkarn</Text>
+      <Text style={myStyle.content}>Creator: {developer.name} | {developer.year}</Text>
       <Image
         source={{
           uri: "https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I",
         }}
         style={myStyle.image}
       ></Image>
-      <Button title="View Detail" color={'red'} onPress={ () => { 
-        console.log('If See this. Button is Work')
-        Alert.alert('If See this. Button is Work')
-        }
-        }></Button>
+      <Button
+        title="Update Detail"
+        color={"red"}
+        onPress={() => {
+          console.log("If See this. Button is Work");
+          setDeveloper({ name:"ARM" , year: 2567});
+        }}
+      ></Button>
     </View>
   );
 }
