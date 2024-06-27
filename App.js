b2bbaeff9f3a70dc6fd3f1ec8fbe9ebc7a8d997d
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-06-27 15:53:30 +0700

    Button

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Text, View, Image, ScrollView } from "react-native";
+import { Text, View, Image, Button } from "react-native";
 import { myStyle } from "./styles/myStyle";
 
 const logo = require("./assets/cat.jpg");
@@ -6,48 +6,16 @@ const logo = require("./assets/cat.jpg");
 export default function App() {
   return (
     <View style={myStyle.container}>
-      <ScrollView>
-        <Image
-          source={require("./assets/cat.jpg")}
-          style={myStyle.image}
-        ></Image>
-        <Text style={myStyle.header}>Hello React Native!!</Text>
-        <Text style={myStyle.content}>Creator: Nattkarn</Text>
-        <Image
-          source={{
-            uri: "https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I",
-          }}
-          style={myStyle.image}
-        ></Image>
-
-        <Text style={myStyle.content}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-          soluta. Debitis nemo dolore incidunt nesciunt consequuntur
-          accusantium. Enim accusamus cum asperiores quam sapiente officia.
-          Quaerat necessitatibus repellendus repellat consectetur dolorum.
-        </Text>
-
-        <Text style={myStyle.content}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-          soluta. Debitis nemo dolore incidunt nesciunt consequuntur
-          accusantium. Enim accusamus cum asperiores quam sapiente officia.
-          Quaerat necessitatibus repellendus repellat consectetur dolorum.
-        </Text>
-
-        <Text style={myStyle.content}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-          soluta. Debitis nemo dolore incidunt nesciunt consequuntur
-          accusantium. Enim accusamus cum asperiores quam sapiente officia.
-          Quaerat necessitatibus repellendus repellat consectetur dolorum.
-        </Text>
-
-        <Text style={myStyle.content}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-          soluta. Debitis nemo dolore incidunt nesciunt consequuntur
-          accusantium. Enim accusamus cum asperiores quam sapiente officia.
-          Quaerat necessitatibus repellendus repellat consectetur dolorum.
-        </Text>
-      </ScrollView>
+      <Image source={require("./assets/cat.jpg")} style={myStyle.image}></Image>
+      <Text style={myStyle.header}>Hello React Native!!</Text>
+      <Text style={myStyle.content}>Creator: Nattkarn</Text>
+      <Image
+        source={{
+          uri: "https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I",
+        }}
+        style={myStyle.image}
+      ></Image>
+      <Button title="View Detail" color={'red'} onPress={ () => { console.log('If See this. Button is Work')}}></Button>
     </View>
   );
 }
