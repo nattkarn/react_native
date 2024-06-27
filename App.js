59bb2d013482279d8152a5a59c66d12c86423f82
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-06-27 15:15:51 +0700

    Style Sheet

--- App.js
+++ App.js
@@ -2,16 +2,27 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: "colum",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{color:'red', fontSize:20,fontWeight:'bold'}}>Hello React Native!!</Text>
-      <Text style={{fontStyle:'italic'}}>Creator: Nattkarn</Text>
+    <View style={myStyle.container}>
+      <Text style={myStyle.header}>Hello React Native!!</Text>
+      <Text style={myStyle.content}>Creator: Nattkarn</Text>
     </View>
   );
 }
+
+const myStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "colum",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'orange'
+  },
+  header: {
+    color: "red",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  content: {
+    fontStyle: "italic",
+  },
+});
