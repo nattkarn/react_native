039b4417ebe5281c6ea8fd64e0d80d19304ce62a
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-06-27 14:28:18 +0700

    view and flexbox

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello my first App</Text>
-      
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: "orange",
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <View style={{ backgroundColor: "blue", width: 80, height: 80, alignSelf:'flex-start'}}></View>
+      <View style={{ backgroundColor: "yellow", width: 80, height: 80, alignSelf:'center' }}></View>
+      <View style={{ backgroundColor: "green", width: 80, height: 80, alignSelf:'flex-end' }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
