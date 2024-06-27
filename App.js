c41e7af413c91bdad98eba15ede5123688da2462
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-06-27 14:37:24 +0700

    Text Component

--- App.js
+++ App.js
@@ -4,16 +4,14 @@ export default function App() {
   return (
     <View
       style={{
-        backgroundColor: "orange",
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
+        flexDirection: "colum",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <View style={{ backgroundColor: "blue", width: 80, height: 80, alignSelf:'flex-start'}}></View>
-      <View style={{ backgroundColor: "yellow", width: 80, height: 80, alignSelf:'center' }}></View>
-      <View style={{ backgroundColor: "green", width: 80, height: 80, alignSelf:'flex-end' }}></View>
+      <Text style={{color:'red', fontSize:20,fontWeight:'bold'}}>Hello React Native!!</Text>
+      <Text style={{fontStyle:'italic'}}>Creator: Nattkarn</Text>
     </View>
   );
 }
